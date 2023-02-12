08d73c91713b1decf0985cf2765e8f67bb670323
abdelino17 <fare.abdel@gmail.com>
2023-02-12 20:34:03 +0100

    feat: update main.py

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 from constructs import Construct
 from cdktf import App, TerraformOutput, TerraformStack
-from cdktf_cdktf_provider_aws import AwsProvider, s3
+from cdktf_cdktf_provider_aws.provider import AwsProvider
+from cdktf_cdktf_provider_aws.s3_bucket import S3Bucket
+
+
 # Define your stack
-
-
 class MyStack(TerraformStack):
     def __init__(self, scope: Construct, ns: str):
         super().__init__(scope, ns)
 
         # Configure the AWS provider to use the eu-west-3 region
-        AwsProvider(self, "AWS", region="eu-west-3")
+        AwsProvider(
+            self,
+            "AWS",
+            region="eu-west-3"
+        )
 
         # Create your first bucket
         # pass the id of the resource and the name of the bucket
         # The code also configures the instance with named arguments, using camel case for properties that correspond to the AWS provider documentation.
-        first_bucket = s3.S3Bucket(
+        first_bucket = S3Bucket(
             self, "first_bucket",
             bucket="blog.abdelfare.me",
             acl="public-read"
